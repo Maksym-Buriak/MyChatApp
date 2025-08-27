afb6dbc97acc244c6bbbb248559834ca8699b1e6
Maks <maksimburiakm@gmail.com>
2025-08-28 00:39:06 +0300

    add: nickname validation

--- app/src/main/java/com/maks_buriak/mychat/presentation/viewmodel/NickNameViewModel.kt
+++ app/src/main/java/com/maks_buriak/mychat/presentation/viewmodel/NickNameViewModel.kt
@@ -19,11 +19,40 @@ class NickNameViewModel(
     fun saveNickName(uid: String, nickName: String) {
         viewModelScope.launch {
             _uiState.value = NickNameUiState.Loading
-            val taken = isNickNameTakenUseCase(nickName)
+
+            val trimmedNick = nickName.trim()
+
+            if (trimmedNick.isBlank()) {
+                _uiState.value = NickNameUiState.Error("Нік не може бути порожнім!")
+                return@launch
+            }
+
+            if (trimmedNick.contains(" ")) {
+                _uiState.value = NickNameUiState.Error("Нік не може містити пробіли")
+                return@launch
+            }
+
+            val regex = Regex("^[A-Za-z0-9_]+$")
+            if (!regex.matches(trimmedNick)) {
+                _uiState.value = NickNameUiState.Error("Нік може містити тільки латинські літери, цифри та символ '_'")
+                return@launch
+            }
+
+            if (trimmedNick.length < 3) {
+                _uiState.value = NickNameUiState.Error("Нік повинен містити щонайменше 3 символи")
+                return@launch
+            }
+
+            if (trimmedNick.length > 20) {
+                _uiState.value = NickNameUiState.Error("Нік не може перевищувати 20 символів")
+                return@launch
+            }
+
+            val taken = isNickNameTakenUseCase(trimmedNick)
             if (taken) {
                 _uiState.value = NickNameUiState.Error("Цей нік вже зайнятий")
             } else {
-                updateUserNickUseCase(uid, nickName)
+                updateUserNickUseCase(uid, trimmedNick)
                 _uiState.value = NickNameUiState.Success
             }
         }
